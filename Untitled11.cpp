929db584d1a2ecf23c0dc5642d7406058990cdd6
Lalith023 <118877220+Lalith023@users.noreply.github.com>
2022-12-07 22:23:16 -0800

    Add files via upload

---
+++ Untitled11.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	float x,y;
+	scanf("%f",&x);
+	y=sqrt(x);
+	print("sqroot of %f is %f \n",x,y)
+}
+
---
+++ add.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()n
+{
+    int num1,num2,a;
+	printf("enter the number:");
+	scanf("%d",&num1,&num2);
+	a=num1+num2;
+	printf("%d+%d=%d",num1,num2);
+	return 0;
+}
